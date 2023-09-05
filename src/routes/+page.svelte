65c5b810f39bfb6fece7670e52e48ab3c9d840b1
Tjips <thomas@scheepers.com>
2023-09-05 12:29:31 +0200

    Rolback

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,10 @@
 <script>
-    export let data
-
     import Taskbar from "$lib/taskbar.svelte";
     import Icon from "$lib/icon.svelte";
     import Terminal from "../lib/terminal.svelte";
 </script>
 
 <main id="main-view">
-    <pre>
-        {JSON.stringify(data, null, 2)}
-    </pre>
-
     <!-- Taskbar at the top of the page -->
     <Taskbar/>
     <!-- Icon of the terminal -->
